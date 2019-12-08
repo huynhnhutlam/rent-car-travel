6e33ac3c301e468fb3ed2d470e580866dc51b293
huynhnhutlam <49746447+huynhnhutlam@users.noreply.github.com>
2019-12-08 23:28:39 +0700

    update

--- rent_car_travel/lib/src/constants/api_http.dart
+++ rent_car_travel/lib/src/constants/api_http.dart
@@ -1,9 +1,11 @@
 class ApiHttp  {
-  static final String api = 'https://fuzzy-bear-92.localtunnel.me';
+  static final String api = 'https://bright-otter-56.localtunnel.me';
   static final String urlListVehicle = api + '/rent_car/vehicle/get_list_vehicle.php';
   static final String urlListRoute =api+ '/rent_car/route/get_list_route.php';
   static final String urlListService = api + '/rent_car/service/get_list_service.php';
   static final String urlListVehicleOpen = api + '/rent_car/vehicle/get_list_vehicle_open.php';
+  static final String urlListUpdateOpen = api + '/rent_car/vehicle/update_vehicle.php';
+  static final String urlListBannerVehicle = api + '/rent_car/vehicle/get_banner_vehicle.php';
   static final String urlLogin = api + '/rent_car/travel_manage/login.php';
   static final String urlRegister = api + '/rent_car/travel_manage/register.php';
   static final String urlBooking = api + '/rent_car/travel_manage/booking.php';
